d0df2f5b52748b7a928418c6675f43219e61c2ac
Anitta Antony <anitta.trinity@gmail.com>
2024-11-25 11:35:00 +0530

    hbs viwe engine add to the app.js

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var hbs=require('express-handlebars')
+var hbs = require ('express-handlebars');
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -13,7 +14,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-app.engine('hbs',hbs({extname:'hbs',defaultLayout:'layout',layoutsDir:__dirname+'/views/layout/',partialsDir:__dirname+'/views/patials/'}))
+app.engine('hbs', hbs.engine({extname:'hbs',defaultLayout:'layout',layoutsDir:__dirname+'/views/layout/',partialsDir:__dirname+'/views/patials/'}))
 
 
 app.use(logger('dev'));
